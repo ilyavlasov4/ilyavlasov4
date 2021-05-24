235c01508d0ae2743b6fd0099d1332f5447484f2
ilyavlasov4 <ilyavlasov1+1@gmail.com>
2021-05-24 17:28:44 -0500

    Homework Ebay

---
+++ data.js
@@ -0,0 +1,195 @@
+const data = {
+    "200 Contour Next Test Strips 4 Boxes of 50 EXP 11/22": {
+        "productTitleText": "200 Contour Next Test Strips 4 Boxes of 50 EXP 11/22"
+    },
+    "Prodigy NO CODING Diabetic Blood Glucose 300 Test Strips (6 boxes) EXP 10/2022": {
+        "productTitleText": "Prodigy NO CODING Diabetic Blood Glucose 300 Test Strips (6 boxes) EXP 10/2022"
+    },
+    "Abbott FreeStyle LITE 50/100 Lite Blood Glucose Test Strips Exp 3-2022": {
+        "productTitleText": "Abbott FreeStyle LITE 50/100 Lite Blood Glucose Test Strips Exp 3-2022"
+    },
+    "100 Contour Test Strips 2 Boxes of 50 ct Exp 12/2021-Freaky Fast Shipping!!!": {
+        "productTitleText": "100 Contour Test Strips 2 Boxes of 50 ct Exp 12/2021-Freaky Fast Shipping!!!"
+    },
+    "NEW LISTINGOne Touch Ultra Test Strips 1 Box of 100 Expires 2021-2022": {
+        "productTitleText": "NEW LISTINGOne Touch Ultra Test Strips 1 Box of 100 Expires 2021-2022"
+    },
+    "2 boxes of Contour test strips 50 count": {
+        "productTitleText": "2 boxes of Contour test strips 50 count"
+    },
+    "NEW LISTINGRare grinderswith test pressing  album southern rock ": {
+        "productTitleText": "NEW LISTINGRare grinderswith test pressing  album southern rock "
+    },
+    "NEW LISTINGCasals Végh PHILIPS 500-120 Beethoven Trio No. 3 In C Minor TEST PRESSING MONO": {
+        "productTitleText": "NEW LISTINGCasals Végh PHILIPS 500-120 Beethoven Trio No. 3 In C Minor TEST PRESSING MONO"
+    },
+    "CANTA MERARI CASTRO LP*!QUE DICHOSA SERE!*CHRISTIAN*DISCOS JERICO*TESTED ALBUM*": {
+        "productTitleText": "CANTA MERARI CASTRO LP*!QUE DICHOSA SERE!*CHRISTIAN*DISCOS JERICO*TESTED ALBUM*"
+    },
+    "NEW LISTINGInterapid 312B-1V dial test indicator .0005": {
+        "productTitleText": "NEW LISTINGInterapid 312B-1V dial test indicator .0005"
+    },
+    "SALE! 250ct test strips, NEW, Contour Next 5-50 CT vials,exp:6/21, FREE SHIPPING": {
+        "productTitleText": "SALE! 250ct test strips, NEW, Contour Next 5-50 CT vials,exp:6/21, FREE SHIPPING"
+    },
+    "NEW LISTINGShure Audio Obstacle Course Test LP": {
+        "productTitleText": "NEW LISTINGShure Audio Obstacle Course Test LP"
+    },
+    "NEW LISTINGSUN KIL MOON Admiral Fell Promises TEST PRESS 2XLP SEALED MINT pressing Kozlek": {
+        "productTitleText": "NEW LISTINGSUN KIL MOON Admiral Fell Promises TEST PRESS 2XLP SEALED MINT pressing Kozlek"
+    },
+    "NEW LISTINGShure Audio Obstacle Course Era III Test LP": {
+        "productTitleText": "NEW LISTINGShure Audio Obstacle Course Era III Test LP"
+    },
+    "TRUE Metrix Blood Glucose Test Strips 150 Count, Exp 07/22 +Freaky Fast Shipping": {
+        "productTitleText": "TRUE Metrix Blood Glucose Test Strips 150 Count, Exp 07/22 +Freaky Fast Shipping"
+    },
+    "TRUE METRIX Blood Glucose Test Strips, Expire 07/22, FREE SHIPPING": {
+        "productTitleText": "TRUE METRIX Blood Glucose Test Strips, Expire 07/22, FREE SHIPPING"
+    },
+    "300 Contour Next Test Strips 6 Boxes of 50ct Exp 07/2022-Freaky Fast Shipping!!!": {
+        "productTitleText": "300 Contour Next Test Strips 6 Boxes of 50ct Exp 07/2022-Freaky Fast Shipping!!!"
+    },
+    "NEW SEALED Free Style Lite 100 Test Strips Exp 6/22 and beyond": {
+        "productTitleText": "NEW SEALED Free Style Lite 100 Test Strips Exp 6/22 and beyond"
+    },
+    "Nintendo DS Lite - Pick Your Color - Tested & Working - Pink Blue Red Black": {
+        "productTitleText": "Nintendo DS Lite - Pick Your Color - Tested & Working - Pink Blue Red Black"
+    },
+    "Accu-Chek Aviva Plus 50 Count Exp:11/30/2021 Diabetic Glucose Test Strips Sealed": {
+        "productTitleText": "Accu-Chek Aviva Plus 50 Count Exp:11/30/2021 Diabetic Glucose Test Strips Sealed"
+    },
+    "OEM N64 Expansion Pack Original Authentic Pak Nintendo 64 TESTED SHIPS FAST!": {
+        "productTitleText": "OEM N64 Expansion Pack Original Authentic Pak Nintendo 64 TESTED SHIPS FAST!"
+    },
+    "Sony PlayStation 3 PS3 Slim Console with Controller & Cables 160gb Tested 2501A": {
+        "productTitleText": "Sony PlayStation 3 PS3 Slim Console with Controller & Cables 160gb Tested 2501A"
+    },
+    "VIBRANT BLUE PSP 3000 System w/ Charger & Memory Card Bundle TESTED WORKS Import": {
+        "productTitleText": "VIBRANT BLUE PSP 3000 System w/ Charger & Memory Card Bundle TESTED WORKS Import"
+    },
+    "PEARL WHITE PSP 3000 System w/ Charger & Memory Card Bundle TESTED WORKS Import": {
+        "productTitleText": "PEARL WHITE PSP 3000 System w/ Charger & Memory Card Bundle TESTED WORKS Import"
+    },
+    "100 One Touch VERIO Test Strips NIB 9/21+ Expires 09/2021 or later 1 BOX": {
+        "productTitleText": "100 One Touch VERIO Test Strips NIB 9/21+ Expires 09/2021 or later 1 BOX"
+    },
+    "NES Game LOT Cleaned and Tested **UPDATED 5/17**": {
+        "productTitleText": "NES Game LOT Cleaned and Tested **UPDATED 5/17**"
+    },
+    "BLACK Sony PSP 3000 System w/ Charger & Memory Card Bundle TESTED WORKS Import": {
+        "productTitleText": "BLACK Sony PSP 3000 System w/ Charger & Memory Card Bundle TESTED WORKS Import"
+    },
+    "*AUTHENTIC* Nintendo 64 N64 Games Zelda Super Mario Kart Banjo 007 TESTED - PICK": {
+        "productTitleText": "*AUTHENTIC* Nintendo 64 N64 Games Zelda Super Mario Kart Banjo 007 TESTED - PICK"
+    },
+    "SteelSeries Arctis Pro Wireless Black Headset For PlayStation 4/PC Tested": {
+        "productTitleText": "SteelSeries Arctis Pro Wireless Black Headset For PlayStation 4/PC Tested"
+    },
+    "One Touch Verio 100 Count,Exp:10/2021+ Glucose Diabetic Test Strips Broken Seal": {
+        "productTitleText": "One Touch Verio 100 Count,Exp:10/2021+ Glucose Diabetic Test Strips Broken Seal"
+    },
+    "Nintendo Gamecube Controller - Black OEM | AUTHENTIC | TESTED": {
+        "productTitleText": "Nintendo Gamecube Controller - Black OEM | AUTHENTIC | TESTED"
+    },
+    "🤖Original Anki Cozmo Blocks Cube 1, 2, & 3 Set w/ New Battery installed -Tested": {
+        "productTitleText": "🤖Original Anki Cozmo Blocks Cube 1, 2, & 3 Set w/ New Battery installed -Tested"
+    },
+    "The Complete America's Test Kitchen TV Show Cookbook Book The Fast Free Shipping": {
+        "productTitleText": "The Complete America's Test Kitchen TV Show Cookbook Book The Fast Free Shipping"
+    },
+    "Bayer Contour NEXT test strips 50ct, exp JUNE 30 2022, DISCOUNT 2+ boxes": {
+        "productTitleText": "Bayer Contour NEXT test strips 50ct, exp JUNE 30 2022, DISCOUNT 2+ boxes"
+    },
+    "VIBRANT RADIANT RED PSP 3000 System w/ Charger, Memory Card Bundle TESTED Import": {
+        "productTitleText": "VIBRANT RADIANT RED PSP 3000 System w/ Charger, Memory Card Bundle TESTED Import"
+    },
+    "Official Genuine Nintendo Switch Console TV Dock Station ONLY Tested! Working!": {
+        "productTitleText": "Official Genuine Nintendo Switch Console TV Dock Station ONLY Tested! Working!"
+    },
+    "NEW LISTINGTodd Edwards-No Scrubs (TLC)-12” single-VG-Very Rare remix-Test Pressing": {
+        "productTitleText": "NEW LISTINGTodd Edwards-No Scrubs (TLC)-12” single-VG-Very Rare remix-Test Pressing"
+    },
+    "ACCU-CHEK SmartView Test strips - 100 X2 = 200 Test Strips Lot of 2": {
+        "productTitleText": "ACCU-CHEK SmartView Test strips - 100 X2 = 200 Test Strips Lot of 2"
+    },
+    "TRUE METRIX Glucose Diabetic Test Strips 300 Strips ( 50CT X 6) Exp 07/22": {
+        "productTitleText": "TRUE METRIX Glucose Diabetic Test Strips 300 Strips ( 50CT X 6) Exp 07/22"
+    },
+    "2 boxes of Contour NEXT test strips 50 count. 100 strips total.": {
+        "productTitleText": "2 boxes of Contour NEXT test strips 50 count. 100 strips total."
+    },
+    "Zonko K105 10.1\" Android Tablet Android 9 - 32GB - Tested": {
+        "productTitleText": "Zonko K105 10.1\" Android Tablet Android 9 - 32GB - Tested"
+    },
+    "GAMECUBE Authentic Games I - P ( Nintendo Gamecube) CLEANED AND TESTED": {
+        "productTitleText": "GAMECUBE Authentic Games I - P ( Nintendo Gamecube) CLEANED AND TESTED"
+    },
+    "ONE TOUCH ULTRA 50 COUNT TEST STRIPS EXP 7/21 FREE SHIP FINAL SALE $$": {
+        "productTitleText": "ONE TOUCH ULTRA 50 COUNT TEST STRIPS EXP 7/21 FREE SHIP FINAL SALE $$"
+    },
+    "EGO 56V Battery BA1400, 2.5AH, Portable Lithium Ion, Tested, Battery Only": {
+        "productTitleText": "EGO 56V Battery BA1400, 2.5AH, Portable Lithium Ion, Tested, Battery Only"
+    },
+    "Nintendo DS Original NTR-001 Console with Charger- Graphite Black - Tested Works": {
+        "productTitleText": "Nintendo DS Original NTR-001 Console with Charger- Graphite Black - Tested Works"
+    },
+    "Full Set Of 7! Pokémon Nintendo GameBoy Color Reproduction, Tested Works!": {
+        "productTitleText": "Full Set Of 7! Pokémon Nintendo GameBoy Color Reproduction, Tested Works!"
+    },
+    "6 Professional Heavy Duty Carabiners-CLIMB X RESCUE-60-12 20KN TEST 30CBCBA": {
+        "productTitleText": "6 Professional Heavy Duty Carabiners-CLIMB X RESCUE-60-12 20KN TEST 30CBCBA"
+    },
+    "TRUE TRACK Blood Glucose 150 Test Strips EXP 03/2023 FREE Shipping.": {
+        "productTitleText": "TRUE TRACK Blood Glucose 150 Test Strips EXP 03/2023 FREE Shipping."
+    },
+    "50 ACCU-CHEK Aviva Plus Test Strips EXP 07/31/2021, One Retail Box, MINT": {
+        "productTitleText": "50 ACCU-CHEK Aviva Plus Test Strips EXP 07/31/2021, One Retail Box, MINT"
+    },
+    "One Touch Verio 50 Ct.Exp:09/2021 Blood Glucose Diabetic Test Strip Factory Seal": {
+        "productTitleText": "One Touch Verio 50 Ct.Exp:09/2021 Blood Glucose Diabetic Test Strip Factory Seal"
+    },
+    "100 OneTouch Verio Diabetic Test Strips (2x50) unopened and unexpired!": {
+        "productTitleText": "100 OneTouch Verio Diabetic Test Strips (2x50) unopened and unexpired!"
+    },
+    "UniStrip Glucose Test Strips 100 ct Generic For One Touch Ultra Strips EXP 12/22": {
+        "productTitleText": "UniStrip Glucose Test Strips 100 ct Generic For One Touch Ultra Strips EXP 12/22"
+    },
+    "TRUE Metrix Blood Glucose TEST STRIPS 250 COUNT. EXP 06/2022, FAST FREE shipping": {
+        "productTitleText": "TRUE Metrix Blood Glucose TEST STRIPS 250 COUNT. EXP 06/2022, FAST FREE shipping"
+    },
+    "BRAND NEW (SEALED) 12/30/2021 ROCHE COAGUCHEK XS PT/INR TEST STRIPS,48/BOX": {
+        "productTitleText": "BRAND NEW (SEALED) 12/30/2021 ROCHE COAGUCHEK XS PT/INR TEST STRIPS,48/BOX"
+    },
+    "EverlyWell Food Sensitivity Kit Test - Brand New Factory Sealed": {
+        "productTitleText": "EverlyWell Food Sensitivity Kit Test - Brand New Factory Sealed"
+    },
+    "TRUE Metrix Glucose Diabetic Test Strips 250Ct Exp3/22 Real Freaky fast shipping": {
+        "productTitleText": "TRUE Metrix Glucose Diabetic Test Strips 250Ct Exp3/22 Real Freaky fast shipping"
+    },
+    "Unistrip 1 Blood Glucose Test Strips 200 Qty. Exp 12/2022 Free shipping": {
+        "productTitleText": "Unistrip 1 Blood Glucose Test Strips 200 Qty. Exp 12/2022 Free shipping"
+    },
+    "One Touch Verio Test Strips, 50 Count each EXP. 06/2022 or later": {
+        "productTitleText": "One Touch Verio Test Strips, 50 Count each EXP. 06/2022 or later"
+    },
+    "True Balance Diabeties Test Strips 100 Strips": {
+        "productTitleText": "True Balance Diabeties Test Strips 100 Strips"
+    },
+    "One Touch Ultra Blue 50 Count, Exp:07/2021+, Diabetic Test Strips Sealed": {
+        "productTitleText": "One Touch Ultra Blue 50 Count, Exp:07/2021+, Diabetic Test Strips Sealed"
+    },
+    "TRUEtrack Glucose Test Strips 200ct (EXP 04/23), 100 Lancets 30G, Lancing Device": {
+        "productTitleText": "TRUEtrack Glucose Test Strips 200ct (EXP 04/23), 100 Lancets 30G, Lancing Device"
+    },
+    "TRUE Metrix Blood Glucose Test Strips Box of 10 X 5 (50 CT) --Depend on US!": {
+        "productTitleText": "TRUE Metrix Blood Glucose Test Strips Box of 10 X 5 (50 CT) --Depend on US!"
+    },
+    "Monster Test Testosterone Booster Testosterona Supplement for Men AM and PM 2 Pk": {
+        "productTitleText": "Monster Test Testosterone Booster Testosterona Supplement for Men AM and PM 2 Pk"
+    }
+}
+console.log(JSON.stringify(data))
+// for (let (key, value) in data {
+//     console.log(data)
+// }
---
+++ specs/login/login-spec.js
@@ -0,0 +1,53 @@
+const loginUrl = 'https://www.ebay.com/';
+const fs = require('fs');
+const { expect } = require('chai');
+
+describe('User should be able to see items', () => {
+    it.only('should Loop through all items once and verify all things inside the loop', () => {
+        // Navigate to ebay.com
+        browser.url(loginUrl);
+        const urlLink = browser.getUrl();
+        expect(urlLink).to.equal('https://www.ebay.com/')
+
+        // Type in “test” in a search textbox
+        const typeTest = 'test';
+        const searchField = $('#gh-ac');
+        searchField.setValue(typeTest);
+
+        // Click Search button
+        const searchBtn = $('#gh-btn');
+        searchBtn.click();
+        const searchResults = $('.srp-controls__count-heading').getText();
+        expect(searchResults).to.contain('test');
+        
+        // Verify that 50 results appear on the results page
+        const productCardHeader = $$('//*[@id="srp-river-results"]/ul/li/div');
+        browser.waitUntil(() => {
+            return productCardHeader.map((elem) => elem.isDisplayed()).length >= 50;
+        }, { timeout: 5000, timeoutMsg: 'Less then 50 products displayed' });
+
+        // Verify that each result contains title with length of 5+ characters
+         const productTitle = [];
+         productCardHeader.forEach(element => {
+                productTitle.push({
+                    productTitleText: element.$('h3').getText(),
+                })
+             })
+            
+            productTitle.forEach(element => {
+                expect(element.productTitleText.length).to.be.above(5);
+            });
+
+        // Verify that each price is a number with a dollar sign in the front of it
+        const productTitlePrice = [];
+        productCardHeader.forEach(element => {
+            productTitlePrice.push({
+            productTitlePrice: element.$('[class="s-item__price"]').getText(),
+            })
+        })
+        productTitlePrice.forEach(element => {
+            expect(element.productTitlePrice).to.be.include('$');
+            expect(parseInt(element.productTitlePrice)).to.be.a('number');
+        });
+    })
+})
